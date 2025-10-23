98f8186a6e06cc123cdf9a24f2f71ffb138ce1c3
MatheusThierri <matheusthierri7@gmail.com>
2025-10-23 20:18:15 -0300

    First commit

---
+++ src/test/java/com/olympofitwear/olympo/olympo_api/OlympoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olympofitwear.olympo.olympo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
